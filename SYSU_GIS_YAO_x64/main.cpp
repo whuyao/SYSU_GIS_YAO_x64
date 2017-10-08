f58a7ecb35ab43a0c7024cacb9be3c67b778137b
Yao Yao <whuyao@foxmail.com>
2017-10-08 11:12:32 +0800

    version v0.1

--- SYSU_GIS_YAO_x64/main.cpp
+++ SYSU_GIS_YAO_x64/main.cpp
@@ -1,8 +1,22 @@
-#include <QtCore/QCoreApplication>
+#include <QtCore>
+#include <iostream>
+#include "gdal_priv.h"
+#include "ogrsf_frmts.h"
+using namespace std;
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+
+	GDALAllRegister();
+	OGRRegisterAll();
+	cout << "GDAL and OGR Register Success!" << endl;
+	
+
+	cout << "hello world!" << endl;
+
+
+
 
 	return a.exec();
 }
